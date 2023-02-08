6e055bbfbf57d61cfadff9350a49a449adc2195f
Vipul Jain <vipulbarjatya@gmail.com>
2023-02-08 15:18:00 +0530

    Frontend HomepageUI complete

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,20 +1,22 @@
-import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import { Container } from "react-bootstrap";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Homescreen from "./components/Homescreen";
+import Productscreen from "./components/Productscreen";
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
       <main className="py-3">
         <Container>
-          <Homescreen />
+          <Route path="/" component={Homescreen} exact />
+          <Route path="/product/:id" component={Productscreen} />
         </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
